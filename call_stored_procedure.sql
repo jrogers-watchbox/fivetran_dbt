1c967dca8717ef489181b5d13afcc01b786c7672
jrogers-watchbox <102746538+jrogers-watchbox@users.noreply.github.com>
2025-11-17 14:54:35 -0500

    Update call_stored_procedure.sql

--- call_stored_procedure.sql
+++ call_stored_procedure.sql
@@ -1,16 +1,3 @@
--- macros/call_stored_procedure.sql
-{% macro call_stored_procedure()  %} 
-
-    
-
- {% set run_sp_operation %}
-
- 
- call dw.proc_dbt_test();
-
- {% endset %}
-
-{% do run_query(run_sp_operation) %}
-
-
-{% endmacro %}
+    {% macro run_proc() %}
+      {% do run_query("call dw.proc_dbt_test();") %}
+    {% endmacro %}
